4ff8cfe11246f635e8cca096bc90c7d55e1789a3
Elias Souza <eliasnsouza1245@gmail.com>
2022-12-05 20:38:43 -0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,29 +1,30 @@
-const { MongoClient } = require("mongodb")
+// const { MongoClient } = require("mongodb")
 
-const uri = "mongodb+srv://elias:elias@cluster0.7acgvi4.mongodb.net/?retryWrites=true&w=majority"
+// const uri = "mongodb+srv://elias:elias@cluster0.7acgvi4.mongodb.net/?retryWrites=true&w=majority"
 
-let cachedDb;
+// let cachedDb;
 
-async function connectToDatabase() {
-  if (cachedDb) {
-    return cachedDb
-  }
-  const client = await MongoClient.connect(uri)
-  const db = client.db("test")
-  cachedDb = db
-  return db
-}
+// async function connectToDatabase() {
+//   if (cachedDb) {
+//     return cachedDb
+//   }
+//   const client = await MongoClient.connect(uri)
+//   const db = client.db("test")
+//   cachedDb = db
+//   return db
+// }
 
 
 export default async function handler(req, res) {
 
-  const db = await connectToDatabase()
-  const collection = await db.collection("devices")
+  // const db = await connectToDatabase()
+  // const collection = await db.collection("devices")
 
   if (req.method === "GET") {
-    const list = await collection.find().toArray()
-    console.log(list);
+    // const list = await collection.find().toArray()
+    // console.log(list);
+    const list = {name: "Elias"}
     return res.status(200).json(list)
   }
-  
+
 }
