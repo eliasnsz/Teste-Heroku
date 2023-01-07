2bb0796036c24fe52a1ede84c294a93496987a24
Elias Souza <eliasnsouza1245@gmail.com>
2023-01-07 01:38:06 -0300

    .

--- pages/reservas/index.jsx
+++ pages/reservas/index.jsx
@@ -22,6 +22,7 @@ export default function Reservas() {
     refetchOnWindowFocus: false
   })
   
+  if (!session) router.push("/login")
   if (isLoading) return <LoadingScreen/>
 
   const thisUserReservations = allReservations
